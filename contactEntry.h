3283fef02fd1134b90d44b22392de91fad2d0964
naf1965 <124084996+naf1965@users.noreply.github.com>
2023-05-03 14:32:12 -0400

    Update contactEntry.h

--- contactEntry.h
+++ contactEntry.h
@@ -8,7 +8,7 @@ using namespace std;
 class contactEntry {
 public:
     contactEntry();
-    contactEntry(sql::SQLString f, sql::SQLString l, sql::SQLString p, sql::SQLString t, sql::SQLString I);
+    contactEntry(sql::SQLString f, sql::SQLString l, sql::SQLString p, sql::SQLString t, sql::SQLString I, sql::SQLString e, sql::SQLString n);
     string text();
     string json();
      
@@ -17,6 +17,8 @@ public:
     string phone;
     string type;
     string ID;
+    string email;
+    string nickname;
 
 private:
 
