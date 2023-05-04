d82d136575025467d9b0f61847ae2704ac3543fa
naf1965 <124084996+naf1965@users.noreply.github.com>
2023-05-03 22:26:43 -0400

    Update contactApp.js

--- contactApp.js
+++ contactApp.js
@@ -25,10 +25,10 @@ $(document).ready(function() {
 // Build output table from comma delimited list
 function formatMatches(json) {
 
-    var result = '<table class="table table-success table-striped""><tr><th>First</th><th>Last</th><th>Phone</th><th>Type</th><th>Email</th><th>Nickname</th><th>Action</th><tr>';
+    var result = '<table class="table table-success table-striped""><tr><th>First</th><th>Last</th><th>Phone</th><th>Type</th><th>Email</th><th>Age</th><th>Action</th><tr>';
     json.forEach(function(entry, i) {
         result += "<tr><td class='first'>" + entry['first'] + "</td><td class='last'>" + entry['last'];
-        result += "</td><td class='phone'>" + entry['phone'] + "</td><td class='type'>" + entry['type'] +  "</td><td class='email'>" + entry['email'] + "</td><td class='nickname'>" + entry['nickname']+"</td>";
+        result += "</td><td class='phone'>" + entry['phone'] + "</td><td class='type'>" + entry['type'] +  "</td><td class='email'>" + entry['email'] + "</td><td class='age'>" + entry['age']+"</td>";
         result += "<td><button type='button' class='btn btn-primary btn-sm edit' data-bs-toggle='modal' data-bs-target='#editContact' ";
         result += "onclick=\"editContact(" + i + ")\">Edit</button> ";
         result += "<button type='button' class='btn btn-primary btn-sm ' onclick=\"deleteContact("+ entry['ID'] +")\">Delete</button></td></tr>";
@@ -71,7 +71,7 @@ function processAdd(results) {
     document.getElementById("addlast").value = "";
     document.getElementById("addphone").value = "";
     document.getElementById("addemail").value = "";
-    document.getElementById("addnickname").value = "";
+    document.getElementById("addage").value = "";
 
     findMatches(" ");
 
@@ -81,7 +81,7 @@ function addContact() {
     console.log("Attempting to add an entry");
     console.log("Firstname:" + $('#addfirst').val());
     $('#searchresults').empty();
-    fetch(baseUrl + '/contact/add/' + $('#addfirst').val() + "/" + $('#addlast').val() + "/" + $('#addphone').val() + "/" + $('#addtype').text() + "/" + $('#addemail').val() + "/" + $('#addnickname').val(), {
+    fetch(baseUrl + '/contact/add/' + $('#addfirst').val() + "/" + $('#addlast').val() + "/" + $('#addphone').val() + "/" + $('#addtype').text() + "/" + $('#addemail').val() + "/" + $('#addage').val(), {
             method: 'get'
         })
         .then(response => response.json())
@@ -105,7 +105,7 @@ function editContact(row) {
 	document.getElementById("editphone").value = contactList[row]["phone"];
 	document.getElementById("edittype").innerText = contactList[row]["type"];
 	document.getElementById("editemail").value = contactList[row]["email"];
-        document.getElementById("editnickname").value = contactList[row]["nickname"];
+        document.getElementById("editage").value = contactList[row]["age"];
 	
 	//Save ID in modal
 	var modal = document.querySelector("#editContact");
@@ -123,7 +123,7 @@ function updateContact() {
     console.log("Attempting to edit an entry:"+id); 
 
     fetch(baseUrl + '/contact/update/' + id + '/' + document.getElementById("editfirst").value 
-    		+ '/' + document.getElementById("editlast").value + '/' + document.getElementById("editphone").value + '/' + document.getElementById("edittype").innerText + '/' + document.getElementById("editemail").value + '/' + document.getElementById("editnickname").value, {
+    		+ '/' + document.getElementById("editlast").value + '/' + document.getElementById("editphone").value + '/' + document.getElementById("edittype").innerText + '/' + document.getElementById("editemail").value + '/' + document.getElementById("editage").value, {
                 method: 'get'
             })
         .then(alert("Record for " + document.getElementById("editfirst").value + ' ' + document.getElementById("editlast").value + " updated"))
