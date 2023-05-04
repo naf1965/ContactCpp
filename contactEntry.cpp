1344fbf3b5178dfa7fbc85b993f259790d06eaf0
naf1965 <124084996+naf1965@users.noreply.github.com>
2023-05-03 23:44:05 -0400

    Update contactEntry.cpp

--- contactEntry.cpp
+++ contactEntry.cpp
@@ -3,6 +3,7 @@
 contactEntry::contactEntry() {
 }
 
+// added last two variables
 contactEntry::contactEntry(sql::SQLString f, sql::SQLString l, sql::SQLString p, sql::SQLString t, sql::SQLString I, sql::SQLString e, sql::SQLString a) {
     first = f;
     last = l;
@@ -13,24 +14,28 @@ contactEntry::contactEntry(sql::SQLString f, sql::SQLString l, sql::SQLString p,
     age = a;
 }
 
+// added last two
 string contactEntry::text() {
-    string result = ID + ". ";
-    result += first + " ";
-    result += last + " ";
-    result += phone + " ";
-    result += type+ " ";
-    result+= email+ " ";
-    result += age;
-    return result;
+	string result = ID + ". ";
+	result += first + " ";
+	result += last + " ";
+	result += phone + " ";
+	result += type + " ";
+	result += email + " ";
+	result += age;
+	return result;
+
 }
 
+// added last two
 string contactEntry::json() {
-    string result = "{\"ID\":\"" + ID + "\",";
-    result += "\"first\":\"" + first + "\",";
-    result += "\"last\":\"" + last + "\",";
-    result += "\"phone\":\"" + phone + "\",";
-    result += "\"type\":\"" + type + "\"}";
-    result += "\"email\":\"" + email + "\"}";
-    result += "\"age\":\"" + age + "\"}";
-    return result;
+	string result = "{\"ID\":\"" + ID + "\",";
+	result += "\"first\":\"" + first + "\",";
+	result += "\"last\":\"" + last + "\",";
+	result += "\"phone\":\"" + phone + "\",";
+	result += "\"type\":\"" + type + "\",";
+	result += "\"email\":\"" + email + "\",";
+	result += "\"age\":\"" + age + "\"}";
+	return result;
+
 }
